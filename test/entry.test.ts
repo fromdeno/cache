0473002c2abc22921d6f78c0d6104a2f24742b8d
Wojciech Pawlik <woj.pawlik@gmail.com>
2021-09-02 15:23:36 +0200

    Ensure that returned paths are absolute

--- test/entry.test.ts
+++ test/entry.test.ts
@@ -1,10 +1,14 @@
-import { assertEquals, assertMatch, cacheHitUrl } from "./shared.ts";
+import { Path } from "../src/deps.deno.ts";
+import { assert, assertEquals, assertMatch, cacheHitUrl } from "./shared.ts";
 import { CacheEntry } from "../src/entry.ts";
 
 Deno.test("CacheEntry -- string hit", async () => {
   const entry = new CacheEntry(cacheHitUrl.href);
   assertMatch(entry.hash, /^[0-9a-f]{64}$/);
   assertEquals(entry.url, cacheHitUrl);
+  assert(Path.isAbsolute(entry.metaPath));
+  assert(Path.isAbsolute(entry.jsPath));
+  assert(Path.isAbsolute(entry.path));
   await Deno.stat(entry.metaPath);
   await Deno.stat(entry.jsPath);
   await Deno.stat(entry.path);
@@ -14,6 +18,9 @@ Deno.test("CacheEntry -- URL hit", async () => {
   const entry = new CacheEntry(cacheHitUrl);
   assertMatch(entry.hash, /^[0-9a-f]{64}$/);
   assertEquals(entry.url, cacheHitUrl);
+  assert(Path.isAbsolute(entry.metaPath));
+  assert(Path.isAbsolute(entry.jsPath));
+  assert(Path.isAbsolute(entry.path));
   await Deno.stat(entry.metaPath);
   await Deno.stat(entry.jsPath);
   await Deno.stat(entry.path);
