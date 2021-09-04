e739b60782e39a92c5566dd439ddccc04073362a
Wojciech Pawlik <woj.pawlik@gmail.com>
2021-09-04 14:19:50 +0200

    Add colors and summary to `dtest`

--- node/dtest.mjs
+++ node/dtest.mjs
@@ -19,36 +19,55 @@
 
 import { pathToFileURL } from "url";
 import { tests } from "deno.ns/tests";
-import "deno.ns/global";
 
 const cwd = pathToFileURL(process.cwd() + "/");
 const failures = [];
+const stats = {
+  passed: 0,
+  ignored: 0,
+};
 
-for (const path of Deno.args) {
+for (const path of process.argv.slice(2)) {
   tests.length = 0;
   const url = new URL(path, cwd);
   await import(url.href);
   process.stdout.write(`running ${tests.length} tests from ${url}\n`);
   for (const test of tests) {
-    process.stdout.write(`test ${test.name} ... `);
-    if (test.ignore === true) {
-      process.stdout.write("ignored\n");
-      continue;
-    }
+    process.stdout.write(`test ${test.name} ...`);
+    const start = Date.now();
     try {
+      if (test.ignore === true) {
+        process.stdout.write("\x1b[33m ignored");
+        stats.ignored++;
+        continue;
+      }
       await test.fn();
-      process.stdout.write("ok\n");
+      process.stdout.write("\x1b[32m ok");
+      stats.passed++;
     } catch (error) {
-      process.exitCode = 1;
-      process.stdout.write("FAILED\n");
+      process.stdout.write("\x1b[31m FAILED");
       failures.push({ test, error });
+    } finally {
+      const finish = Date.now();
+      process.stdout.write(`\x1b[90m (${finish - start}ms)\n\x1b[0m`);
     }
   }
 }
 
 if (failures.length) {
-  process.stdout.write(`\n${failures.length} tests failed:\n`);
+  process.stdout.write(`\nfailures:\n`);
   for (const { test, error } of failures) {
     process.stdout.write(`\n${test.name}\n${error.stack}\n`);
   }
+  process.stdout.write("\ntest result:\x1b[31m FAILED\x1b[0m. ");
+  process.exitCode = 1;
+} else {
+  process.stdout.write("\ntest result:\x1b[32m ok\x1b[0m. ");
 }
+
+process.stdout.write(`\
+${stats.passed} passed; \
+${failures.length} failed; \
+${stats.ignored} ignored \
+\x1b[90m(${performance.now().toFixed(0)}ms)\x1b[0m
+`);
